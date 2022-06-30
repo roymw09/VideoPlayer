86c21f0435269082db221cd3f0e53c5ff5476aec
roymw09 <60860251+roymw09@users.noreply.github.com>
2022-06-30 15:17:38 -0230

    Added VideoRepository class to retrieve Videos from the given api

---
+++ app/src/main/java/com/silverorange/videoplayer/repository/VideoRepository.java
@@ -0,0 +1,45 @@
+package com.silverorange.videoplayer.repository;
+
+import androidx.lifecycle.MutableLiveData;
+
+import com.silverorange.videoplayer.model.Video;
+import com.silverorange.videoplayer.service.RetrofitService;
+import com.silverorange.videoplayer.service.RetrofitVideoApi;
+import java.util.ArrayList;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class VideoRepository {
+    private static VideoRepository videoRepository;
+    private RetrofitVideoApi videoApi;
+
+    public static VideoRepository getInstance() {
+        if (videoRepository == null) {
+            videoRepository = new VideoRepository();
+        }
+        return videoRepository;
+    }
+
+    public VideoRepository() {
+        videoApi = RetrofitService.createService(RetrofitVideoApi.class);
+    }
+
+    public MutableLiveData<ArrayList<Video>> getVideos() {
+        MutableLiveData<ArrayList<Video>> videoData = new MutableLiveData<>();
+        videoApi.getVideoList().enqueue(new Callback<ArrayList<Video>>() {
+            @Override
+            public void onResponse(Call<ArrayList<Video>> call, Response<ArrayList<Video>> response) {
+                if (response.isSuccessful()) {
+                    videoData.setValue(response.body());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ArrayList<Video>> call, Throwable t) {
+                System.err.println("CAUSE: " + t.getCause());
+            }
+        });
+        return videoData;
+    }
+}
