4f02c73c96ee29c01cb928c11043e4c442985521
roymw09 <60860251+roymw09@users.noreply.github.com>
2022-06-30 15:02:17 -0230

    Added RetrofitService and RetrofitVideoApi to retrieve data from the given api

---
+++ app/src/main/java/com/silverorange/videoplayer/service/RetrofitVideoApi.java
@@ -0,0 +1,13 @@
+package com.silverorange.videoplayer.service;
+
+import com.silverorange.videoplayer.model.Video;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface RetrofitVideoApi {
+    @GET("videos")
+    Call<ArrayList<Video>> getVideoList();
+}
